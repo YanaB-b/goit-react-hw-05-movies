412f72f90abfe527744fc6df2b031bc117fabbec
YanaB-b <j.biliuk96@gmail.com>
2023-04-30 20:14:12 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,9 @@ import Layout from './Layout/Layout';
 
 const Home = lazy(() => import('pages/Home/Home'));
 const Movies = lazy(() => import('pages/Movies/Movies'));
-// const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
-// const Cast = lazy(() => import('components/Cast/Cast'));
-// const Reviews = lazy(() => import('components/Reviews/Reviews'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 export const App = () => {
   return (
@@ -15,10 +15,10 @@ export const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
-          {/* <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
-          </Route> */}
+          </Route>
         </Route>
       </Routes>
     </>
